028b34e2a29d1857c1553cf8bf17c889a644bf9e
Emanuele Messina <emanuelemessina.em@gmail.com>
2025-01-31 20:59:17 +0100

    fixed eval loop

--- model/launch.py
+++ model/launch.py
@@ -407,50 +407,49 @@ def visualize_predictions(images, boxes, scores, super_labels, sub_labels):
 
 SUPPRESSIONMAXXING_THRESH = 0.9
 
-if eval == 'train':
-    print("Evaluating on Train set")
+print(f"Evaluating on {eval} set")
 
-    while True:
+while True:
 
-        images_targets = [dataset[random.randint(0, len(dataset) - 1)] for _ in range(3)]
-        images, targets = zip(*images_targets)
+    images_targets = [dataset[random.randint(0, len(dataset) - 1)] for _ in range(3)]
+    images, targets = zip(*images_targets)
 
-        outputs = model([img.to(device) for img in images])
+    outputs = model([img.to(device) for img in images])
 
-        pred_boxes = []
-        pred_scores = []
-        boxes_per_image = []
-        for output, target in zip(outputs, targets):
-            boxes = output["boxes"]
-            scores = output["scores"].detach().cpu()
+    pred_boxes = []
+    pred_scores = []
+    boxes_per_image = []
+    for output, target in zip(outputs, targets):
+        boxes = output["boxes"]
+        scores = output["scores"].detach().cpu()
 
-            # low thresh suppresh
-            filtered_boxes = torch.empty(size=(0, 4)).to(device)
-            filtered_scores = []
-            for i, score in enumerate(scores):
-                if score >= SUPPRESSIONMAXXING_THRESH:
-                    filtered_boxes = torch.cat((filtered_boxes, boxes[i].reshape(1, -1)))
-                    filtered_scores.append(float(score))
+        # low thresh suppresh
+        filtered_boxes = torch.empty(size=(0, 4)).to(device)
+        filtered_scores = []
+        for i, score in enumerate(scores):
+            if score >= SUPPRESSIONMAXXING_THRESH:
+                filtered_boxes = torch.cat((filtered_boxes, boxes[i].reshape(1, -1)))
+                filtered_scores.append(float(score))
 
-            pred_boxes.append(filtered_boxes)
-            pred_scores.append(filtered_scores)
-            boxes_per_image.append(len(filtered_boxes))
+        pred_boxes.append(filtered_boxes)
+        pred_scores.append(filtered_scores)
+        boxes_per_image.append(len(filtered_boxes))
 
-        features = model.roi_heads.box_roi_pool.features
-        image_shapes = model.roi_heads.box_roi_pool.image_shapes
-        box_features = multiscale_roi_align(features, pred_boxes, image_shapes)
-        custom_classifier = model.roi_heads.box_predictor.custom_forward
-        super_logits, sub_logits = custom_classifier(box_features)
-        super_scores = F.softmax(super_logits, -1)
-        sub_scores = F.softmax(sub_logits, -1)
-        super_labels = torch.argmax(super_scores, dim=1).detach().cpu().split(boxes_per_image, 0)
-        sub_labels = torch.argmax(sub_scores, dim=1).detach().cpu().split(boxes_per_image, 0)
+    features = model.roi_heads.box_roi_pool.features
+    image_shapes = model.roi_heads.box_roi_pool.image_shapes
+    box_features = multiscale_roi_align(features, pred_boxes, image_shapes)
+    custom_classifier = model.roi_heads.box_predictor.custom_forward
+    super_logits, sub_logits = custom_classifier(box_features)
+    super_scores = F.softmax(super_logits, -1)
+    sub_scores = F.softmax(sub_logits, -1)
+    super_labels = torch.argmax(super_scores, dim=1).detach().cpu().split(boxes_per_image, 0)
+    sub_labels = torch.argmax(sub_scores, dim=1).detach().cpu().split(boxes_per_image, 0)
 
-        visualize_predictions(images, pred_boxes, pred_scores, super_labels, sub_labels)
+    visualize_predictions(images, pred_boxes, pred_scores, super_labels, sub_labels)
 
-        answer = input("Do you want to see other images? (y/n, default is y): ").strip().lower()
+    answer = input("Do you want to see other images? (y/n, default is y): ").strip().lower()
 
-        if answer not in ['y', '']:
-            break
+    if answer not in ['y', '']:
+        break
 
-    evaluate_detection(dataloader)
+evaluate_detection(dataloader)
